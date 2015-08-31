a856fa4bf97a298bd882672e58c23ed4bf9229bb
valeriabladinieresjo <valeriabladinieresjo@users.noreply.github.com>
2015-08-31 16:16:03 -0500

    Update sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -1,6 +1,6 @@
 //Input Example
 
-_INPUT = "2\n3 9\n17 24";
+//_INPUT = "2\n3 9\n17 24";
 
 //Expected Output
 /*
@@ -22,3 +22,31 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+
+var numeros = _INPUT.split(/\n| /);
+num=numeros[0];
+var min= new Array((numeros.length-1)/2);
+var max= new Array((numeros.length-1)/2);
+cont=0;
+for(k=1; k<numeros.length; k+=2) {
+		min[cont]= parseInt(numeros[k]);
+		cont++;
+	}
+cont=0;
+for(k=2; k<numeros.length; k+=2) {
+		max[cont]= parseInt(numeros[k]);
+		cont++;
+	}
+
+for(r=0; r<num; r++) {
+	o=0;
+	for ( k=min[r]; k<=max[r]; k++){
+		y=0;
+		x= Math.sqrt(k);
+		y= x % 2;
+		if (y==0 || y==1){
+			o++;
+		}
+	}
+	console.log(o);
+}
